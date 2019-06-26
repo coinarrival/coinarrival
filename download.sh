acaa94709cb91555b157fb195c168d4c3c3d6dda
xwy27 <xuwy27@mail2.sysu.edu.cn>
2019-06-26 13:08:19 +0800

    update bash script

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 # set up source code directory
 cd ..
-mkdir coinarrival
+mkdir coinarrival_v1.0
 
 echo "Downloading Source Code..."
-
+cd coinarrival_v1.0
 # FrontEnd code
 git clone https://github.com/coinarrival/FrontEnd.git
 
@@ -13,7 +13,7 @@ git clone https://github.com/coinarrival/ServerEnd.git
 # BackEnd code
 git clone https://github.com/coinarrival/BackEnd.git
 
-mkdir ../ServerEnd/resources/public/dist
-mkdir ../ServerEnd/resources/dist
+mkdir ./ServerEnd/resources/public/dist
+mkdir ./ServerEnd/resources/dist
 
 echo "Download Finish."
--- install.sh
+++ install.sh
@@ -2,31 +2,33 @@
 set -e # once broken, exit bash
 
 echo "Building Repo"
+
 # Build FrontEnd
-cd FrontEnd
 echo "Building FrontEnd Page..."
+
+cd ../coinarrival_v1.0/FrontEnd
 npm install
 npm run build
 
-cp index.html ../ServerEnd/resources/public/index.html
-cp -r ./dist/ ../ServerEnd/resources/
-cp -r ./static/ ../ServerEnd/resources/public/static
-mv ../ServerEnd/resources/dist/*.js ../ServerEnd/resources/public/dist/*.js
+cp index.html ../coinarrival_v1.0/ServerEnd/resources/public/index.html
+cp -r ./dist/ ../coinarrival_v1.0/ServerEnd/resources/
+cp -r ./static/ ../coinarrival_v1.0/ServerEnd/resources/public/static
+mv ../coinarrival_v1.0/ServerEnd/resources/dist/*.js ../coinarrival_v1.0/ServerEnd/resources/public/dist/*.js
 
 echo "Build Finish"
 
 # Build BackEnd
-cd ../BackEnd
 echo "Building BackEnd..."
 
+cd ../coinarrival_v1.0/BackEnd
 docker-compose build
 
 echo "Build Finish"
 
 # Build ServerEnd
-cd ../ServerEnd
 echo "Building ServerEnd..."
 
+cd ../coinarrival_v1.0/ServerEnd
 docker-compose up
 
 echo "Build Finish"
